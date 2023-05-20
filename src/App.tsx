bf9dbbafc705aca459447bf38d2664c85dc8cc80
Randal Vance Cunanan <randal@randalvance.net>
2023-05-20 13:00:07 +0800

    Add Styling and add Field to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css';
-import React from "react";
+import React from 'react';
+import { Field } from './components/Field';
 
 const App: React.FC = () => {
   return (
     <>
       <div>
         <h1>Auto-Driving Car Simulation</h1>
+        <Field width={10} height={10} />
       </div>
     </>
   );
