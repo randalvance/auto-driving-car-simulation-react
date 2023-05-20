99429ab4142c12c2fb0fa38a0c12fe64cb44dcf9
Randal Vance Cunanan <randal@randalvance.net>
2023-05-20 12:59:33 +0800

    Implement Test

--- src/components/Field/Field.tsx
+++ src/components/Field/Field.tsx
@@ -1,10 +1,18 @@
-import React from "react";
+import React from 'react';
+
+import styles from './styles.module.css';
 
 interface Props {
-    width: number;
-    height: number;
+  width: number;
+  height: number;
 }
 
-export const Field: React.FC<Props> = () => {
-    return <h1 role="heading">Hello World</h1>
-}
+export const Field: React.FC<Props> = ({ width, height }) => {
+  return (
+    <div
+      className={styles.field}
+      role="field"
+      style={{ width: width * 100, height: height * 100 }}
+    ></div>
+  );
+};
