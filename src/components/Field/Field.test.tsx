0f235a5795e5e38bcb6620e82eac031f92c076db
Randal Vance Cunanan <randal@randalvance.net>
2023-05-20 12:59:22 +0800

    Test: Renders the field with correct height and width

--- src/components/Field/Field.test.tsx
+++ src/components/Field/Field.test.tsx
@@ -4,11 +4,24 @@ import { render, screen } from '@testing-library/react';
 import { Field } from './Field';
 
 describe('Field', () => {
-  it('should render Field', async () => {
-    render(<Field height={10} width={10} />);
+  describe('should render field with correct height and width', () => {
+    [
+      { width: 1, height: 2, expectedHtmlWidth: 100, expectedHtmlHeight: 200 },
+      { width: 3, height: 3, expectedHtmlWidth: 300, expectedHtmlHeight: 300 },
+      {
+        width: 10,
+        height: 5,
+        expectedHtmlWidth: 1000,
+        expectedHtmlHeight: 500,
+      },
+    ].forEach(({ width, height, expectedHtmlWidth, expectedHtmlHeight }) => {
+      it(`when width=${width} and height=${height}`, async () => {
+        render(<Field width={width} height={height} />);
 
-    await screen.findByRole('heading');
-
-    expect(screen.getByRole('heading')).toHaveTextContent('Hello World');
+        const field = screen.getByRole('field');
+        expect(field.style.height).toBe(`${expectedHtmlHeight}px`);
+        expect(field.style.width).toBe(`${expectedHtmlWidth}px`);
+      });
+    });
   });
 });
