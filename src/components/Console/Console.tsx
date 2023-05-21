59b8e2475ac11c474b136f830f142fbbd738609e
Randal Vance Cunanan <randal@randalvance.net>
2023-05-21 13:48:36 +0800

    Polishes to console

--- src/components/Console/Console.tsx
+++ src/components/Console/Console.tsx
@@ -10,11 +10,21 @@ export const Console: React.FC<Props> = ({ messages }) => {
   const [dispatchCommand] = useStore((s) => [s.dispatchCommand]);
   const [command, setCommand] = useState<string>('');
   const inputRef = useRef<HTMLInputElement>(null);
+  const containerRef = useRef<HTMLDivElement>(null);
   useEffect(() => {
     inputRef.current?.focus();
   }, []);
+
+  useEffect(() => {
+    inputRef.current?.focus();
+    containerRef.current?.scrollTo(0, containerRef.current.scrollHeight);
+  }, [messages]);
   return (
-    <div className={styles.container}>
+    <div
+      ref={containerRef}
+      className={styles.container}
+      onClick={() => inputRef.current?.focus()}
+    >
       {messages.map((message, index) => (
         <div role="message" key={index} className={styles.message}>
           {message}
