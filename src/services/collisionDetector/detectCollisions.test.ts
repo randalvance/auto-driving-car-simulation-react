c4bc4cb0198a4afb0082bca121fe7cb0d54e30cb
Randal Vance Cunanan <randal@randalvance.net>
2023-06-25 12:58:01 +0800

    Collision Detection: Refactor

--- src/services/collisionDetector/detectCollisions.test.ts
+++ src/services/collisionDetector/detectCollisions.test.ts
@@ -1,4 +1,4 @@
-import { type CollisionInfo, type Car } from '@/types';
+import { type Car } from '@/types';
 import { detectCollisions } from './detectCollisions';
 
 const baseCar = {
@@ -22,6 +22,7 @@ it('should detect all cars that a car have collided with', () => {
     x: 1,
     y: 1,
     direction: 'E',
+    collisionInfo: [{ carName: 'car4', step: 0 }],
   };
   const car3: Car = {
     ...baseCar,
@@ -35,14 +36,35 @@ it('should detect all cars that a car have collided with', () => {
     x: 1,
     y: 1,
     direction: 'S',
+    collisionInfo: [{ carName: 'car2', step: 0 }],
   };
   const cars: Car[] = [car1, car2, car3, car4];
   const currentStep = 1;
 
-  const carWithCollisionInfo = detectCollisions(car1.name, cars, currentStep);
+  const carsWithCollisionInfo = detectCollisions(car1.name, cars, currentStep);
 
-  expect(carWithCollisionInfo.collisionInfo).toEqual([
-    { carName: car2.name, step: currentStep } satisfies CollisionInfo,
-    { carName: car4.name, step: currentStep } satisfies CollisionInfo,
+  expect(carsWithCollisionInfo).toEqual([
+    {
+      ...car1,
+      collisionInfo: [
+        { carName: car2.name, step: currentStep },
+        { carName: car4.name, step: currentStep },
+      ],
+    },
+    {
+      ...car2,
+      collisionInfo: [
+        { carName: car4.name, step: 0 },
+        { carName: car1.name, step: currentStep },
+      ],
+    },
+    car3,
+    {
+      ...car4,
+      collisionInfo: [
+        { carName: car2.name, step: 0 },
+        { carName: car1.name, step: currentStep },
+      ],
+    },
   ]);
 });
--- src/services/collisionDetector/detectCollisions.ts
+++ src/services/collisionDetector/detectCollisions.ts
@@ -1,17 +1,17 @@
 import { type CollisionInfo, type Car } from '@/types';
 import { produce } from 'immer';
 
-/** Get information of which cars have collided. Returns the car with collision info.
+/** Get information of which cars have collided. Returns the cars with collision info.
  * @param carName The name of the car to check for collisions
  * @param cars All the cars in the simulation (may include the car to check for collisions)
  * @param step The current step of the simulation
- * @returns The names of all the cars the car has collided with
+ * @returns The list of cars with updated collision info
  */
 export const detectCollisions = (
   carName: Car['name'],
   cars: Car[],
   step: number,
-): Car => {
+): Car[] => {
   const carIndex = cars.findIndex((car) => car.name === carName);
 
   if (carIndex === -1) throw new Error(`Car ${carName} not found`);
@@ -19,19 +19,38 @@ export const detectCollisions = (
   const car = cars[carIndex];
   const collisionInfo: CollisionInfo[] = [];
 
-  for (const otherCar of cars) {
-    // Don't check for collisions with itself
-    if (otherCar.name === carName) continue;
+  const carsWithCollisionInfo: Car[] = [];
 
-    if (car.x === otherCar.x && car.y === otherCar.y) {
-      collisionInfo.push({
-        carName: otherCar.name,
-        step: step,
-      });
+  for (const otherCar of cars) {
+    const hasCollided = car.x === otherCar.x && car.y === otherCar.y;
+
+    // Check if no collisions
+    if (otherCar.name === carName || !hasCollided) {
+      carsWithCollisionInfo.push(otherCar);
+      continue;
     }
+
+    // At this point, we have a collision
+
+    // Add to the car's collision info
+    collisionInfo.push({
+      carName: otherCar.name,
+      step: step,
+    });
+
+    // Update other car to register collision
+    carsWithCollisionInfo.push(
+      produce(otherCar, (draft) => {
+        draft.collisionInfo = draft.collisionInfo ?? [];
+        draft.collisionInfo.push({
+          carName: car.name,
+          step: step,
+        });
+      }),
+    );
   }
 
-  return produce(car, (draft) => {
-    draft.collisionInfo = collisionInfo;
+  return produce(carsWithCollisionInfo, (draft) => {
+    draft[carIndex].collisionInfo = collisionInfo;
   });
 };
